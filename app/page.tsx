9066471a1e533ab5d093834f5ec3947be1bae77b
knoxfounder9-spec <knoxfounder9@gmail.com>
2026-03-01 19:47:04 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      background: "#0f0f0f",
+      color: "white"
+    }}>
+      <h1>Grind Team Portal 🚀</h1>
+    </main>
+  );
+}
